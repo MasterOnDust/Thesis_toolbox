c801428c617b8d7dc8a229109a7868b2e4e2df98
Ove <ovehaugv@outlook.com>
2021-03-23 09:41:42 +0100

    move scripts from ERA5 workflow to thesis_toolbox

--- thesis_toolbox/composites/setup_thesis_data.py
+++ thesis_toolbox/composites/setup_thesis_data.py
@@ -2,7 +2,7 @@ import xarray as xr
 from .create_composites import create_composite, calculate_climatology
 import numpy as np
 
-def mslp_850hpa_wind_composite(mslp, u_wind_850, v_wind_850,strong_years, weak_years, 
+def mslp_wind_composite(mslp, u_wind, v_wind,strong_years, weak_years, plevel,
                                season='', location='', kind=''):
     composite_ds = xr.Dataset()
     composite_ds['msl'] = create_composite(mslp['msl'], weak_years,strong_years)
@@ -10,7 +10,7 @@ def mslp_850hpa_wind_composite(mslp, u_wind_850, v_wind_850,strong_years, weak_y
     composite_ds['msl'].attrs['units'] = 'hPa' 
     composite_ds['u'] = create_composite(u_wind_850['u'], weak_years, strong_years)
     composite_ds['v'] = create_composite(v_wind_850['v'], weak_years, strong_years)
-    composite_ds.attrs['title'] = "Composites of Mslp and windspeed at 850hPa"
+    composite_ds.attrs['title'] = "Composites of Mslp and windspeed at "+ plevel + " hPa"
     composite_ds.attrs['season'] = season
     composite_ds.attrs['source'] = 'ERA5 reanalysis'
     composite_ds.attrs['location'] = location
@@ -37,6 +37,7 @@ def geopot_wind_composite(geopot, u_wind, v_vind, weak_years, strong_years, plev
     composite_ds.attrs['source'] = 'ERA5 reanalysis'
     composite_ds.attrs['location'] = location
     composite_ds.attrs['kind'] = kind
+    return composite_ds
     
 
 def geopot_wind_climatology(geopot, u_wind, v_vind, plevel,start_year=None, end_year=None, season=''):
@@ -54,13 +55,3 @@ def geopot_wind_climatology(geopot, u_wind, v_vind, plevel,start_year=None, end_
     clim_ds.attrs['source'] = 'ERA5 reanalysis'
     return clim_ds
 
-def mslp_850hPa_wind_composite(mslp, u_wind_850, v_wind_850, start_year=None,end_year=None, season=''):
-    clim_ds = xr.Dataset()
-    clim_ds['msl'] = calculate_climatology(mslp['msl'], start_year, end_year)
-    clim_ds['u'] = calculate_climatology(u_wind_850['u'],start_year, end_year)
-    clim_ds['v'] = calculate_climatology(v_wind_850['v'], start_year, end_year)
-    clim_ds.attrs['title'] = "Climatology of mean sea level pressure and windspeed at 850"
-    clim_ds.attrs['season'] = season
-    clim_ds.attrs['source'] = 'ERA5 reanalysis'
-    
-    return clim_ds
--- thesis_toolbox/plot/tools.py
+++ thesis_toolbox/plot/tools.py
@@ -2,6 +2,7 @@ from cartopy.mpl.gridliner import LongitudeFormatter, LatitudeFormatter
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs 
 from matplotlib.ticker import ScalarFormatter,AutoMinorLocator
+import matplotlib
 
 def map_large_scale(ax):
     ax.set_extent([20,180,0,90], crs=ccrs.PlateCarree())
@@ -15,3 +16,48 @@ def map_large_scale(ax):
     ax.yaxis.set_minor_locator(  AutoMinorLocator(3))
     ax.xaxis.set_minor_locator(  AutoMinorLocator(2))
     ax.coastlines()
+    
+    
+class OOMFormatter(matplotlib.ticker.ScalarFormatter):
+    """
+    https://stackoverflow.com/questions/42656139/set-scientific-notation-with-fixed-exponent-and-significant-digits-for-multiple
+    """
+    def __init__(self, order=0, fformat="%1.1f", offset=True, mathText=True):
+        self.oom = order
+        self.fformat = fformat
+        matplotlib.ticker.ScalarFormatter.__init__(self,useOffset=offset,useMathText=mathText)
+    def _set_orderOfMagnitude(self, nothing=None):
+        self.orderOfMagnitude = self.oom
+    def _set_format(self, vmin=None, vmax=None):
+        self.format = self.fformat
+        if self._useMathText:
+            self.format = '$%s$' % self.format
+            
+
+def latex_plot():
+    plt.rcParams.update({'figure.autolayout': True})
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.size'] = 18
+    plt.rcParams['axes.linewidth'] = 2
+    plt.rcParams['legend.frameon'] = False
+    plt.rcParams['axes.linewidth'] = 2
+
+    # --- Use latex-style on text in plots
+    plt.rcParams['text.usetex'] = False
+
+    # --- Custumize the length of the labels
+    plt.rcParams["legend.labelspacing"] = 0.2
+    plt.rcParams["legend.handlelength"] = 1.0
+    plt.rcParams["legend.borderaxespad"] = 0.01
+
+    plt.rcParams['axes.labelsize'] = 16
+    plt.rcParams['axes.titlesize'] = plt.rcParams['font.size']
+    plt.rcParams['legend.fontsize'] = plt.rcParams['font.size']
+    plt.rcParams['xtick.labelsize'] = 14
+    plt.rcParams['ytick.labelsize'] = 14
+    
+    # --- Ignore warnings for generated plot
+    plt.rcParams.update({'figure.max_open_warning': 0})
+
+    plt.linewidth=17.0
+
