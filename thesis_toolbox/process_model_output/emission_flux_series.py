a43c51b020c5d47b237b749c747feda42b812dff
Ove Haugvaldstad <ovehaugv@outlook.com>
2022-06-16 16:01:23 +0200

    fix old package name

--- thesis_toolbox/process_model_output/emission_flux_series.py
+++ thesis_toolbox/process_model_output/emission_flux_series.py
@@ -1,12 +1,12 @@
 import xarray as xr
-import DUST
+import dust
 from netCDF4 import num2date, date2num
 import numpy as np
 
 def create_timeseries(path, lon0=None,lon1=None,lat0=None,lat1=None):
     ds = xr.open_dataset(path)
     with xr.set_options(keep_attrs=True):
-        ds = DUST.utils.utils.region_slice(ds, lon0, lon1, lat0,lat1)
+        ds = dust.utils.utils.region_slice(ds, lon0, lon1, lat0,lat1)
     ems=ds.Emission*ds.area #multiply by area to get units of kg
     ems = ems.sum(dim=['lon','lat'])
     ems.attrs['units']='kg'
@@ -29,32 +29,32 @@ def resample_emission_flux(paths, lon0=None,lon1=None,
     """
     DESCRIPTION:
     ===========
-        Resample data to monthly or yearly frequency, if frequency is different from 
+        Resample data to monthly or yearly frequency, if frequency is different from
         monthly ('m') cum_emission variable will be set as the Emission variable.
-        
-    
+
+
     """
     dsets=[]
-    d0= DUST.read_flexdust_output(paths[0])
+    d0= dust.read_flexdust_output(paths[0])
     start_date=d0.startdate
     starthour=d0.starthour
     for path in paths:
-        temp_ds=DUST.read_flexdust_output(path)
-        temp_ds=DUST.utils.utils.region_slice(temp_ds, lon0,lon1,lat0,lat1)
+        temp_ds=dust.read_flexdust_output(path)
+        temp_ds=dust.utils.utils.region_slice(temp_ds, lon0,lon1,lat0,lat1)
 
         if frequency=='m':
             resampled_ds=temp_ds.Emission.resample(time='m').sum().to_dataset(name='Emission')
             resampled_ds['Emission'].attrs['cell_methods']='time: summed'
-            resampled_ds['Emission'].attrs['units']='kg/m2' 
+            resampled_ds['Emission'].attrs['units']='kg/m2'
             resampled_ds['Emission'].attrs['long_name']='monlthy simulated accumulated dust emissions'
-            
+
             daily = temp_ds.time.resample(time='d').asfreq()
             month_start = list(daily.where(daily.dt.is_month_start,drop=True))
             month_start[0] = temp_ds.time[0]
 
             month_start= [date2num(month.dt,units='days since 1979-1-1', calendar='standard') for month in month_start]
             time_bnds=[date2num(t.time.dt, units='days since 1979-1-1', calendar='standard') for t in resampled_ds.time]
-       
+
             time=xr.DataArray(data=month_start,
                     dims=['time'],
                     coords=dict(time=month_start),
@@ -62,7 +62,7 @@ def resample_emission_flux(paths, lon0=None,lon1=None,
                         units='days since 1979-1-1',
                         bounds='time_bnds')
             )
-                
+
             resampled_ds=resampled_ds.assign(time=time)
             bnds=np.array((month_start,time_bnds)).transpose()
             time_bnds=xr.DataArray(data=bnds,
@@ -77,7 +77,7 @@ def resample_emission_flux(paths, lon0=None,lon1=None,
         else:
             resampled_ds=temp_ds.cum_emission.to_dataset(name='Emission')
             resampled_ds['Emission'].attrs['cell_methods']='time: summed'
-            resampled_ds['Emission'].attrs['units']='kg/m2'   
+            resampled_ds['Emission'].attrs['units']='kg/m2'
             resampled_ds['Emission'].attrs['long_name']='yearly accumulated simulated dust emissions'
             t0 = date2num(temp_ds.time[0].dt, units='days since 1979-1-1', calendar='standard')
             time=xr.DataArray(data=t0,
@@ -88,7 +88,7 @@ def resample_emission_flux(paths, lon0=None,lon1=None,
                         bounds='time_bnds'
                     )
             )
-            
+
             resampled_ds=resampled_ds.assign(time=time)
             t1 = date2num(temp_ds.time[-1].dt, units='days since 1979-1-1', calendar='standard')
             bnds=np.array([t0, t1]).reshape(1,2)
@@ -120,10 +120,10 @@ def resample_emission_flux(paths, lon0=None,lon1=None,
     ds.attrs['lon1']=lon1
     ds.attrs['lat1']=lat1
     ds.attrs['varName']='Emission'
-    
+
     return ds
-    
-    
-                                
-        
-            
+
+
+
+
+
--- thesis_toolbox/process_model_output/process_source_contribution.py
+++ thesis_toolbox/process_model_output/process_source_contribution.py
@@ -1,11 +1,11 @@
 import xarray as xr
-import DUST
+import dust
 
 def create_timeseries(ds, lon0=None,lon1=None,lat0=None,lat1=None):
     with xr.set_options(keep_attrs=True):
-        ds = DUST.utils.utils.region_slice(ds, lon0, lon1, lat0,lat1)
+        ds = dust.utils.utils.region_slice(ds, lon0, lon1, lat0,lat1)
     ts = ds[ds.varName].sum(dim=['lon','lat'], keep_attrs=True)
-    
+
     ts = ts.to_dataset(name=ds.varName)
     ts.attrs=ds.attrs
     if lon0:
